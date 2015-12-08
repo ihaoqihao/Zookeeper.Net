0e561cd1eddc4eb28fe1105a62fcb6d0a56124b7
devhong <jon.hong@qq.com>
2015-12-08 16:33:01 +0800

    add

---
+++ Zookeeper/WatcherAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sodao.Zookeeper
+{
+    /// <summary>
+    /// watcher action
+    /// </summary>
+    public sealed class WatcherAction : IWatcher
+    {
+        private readonly Action<Data.WatchedEvent> _callback = null;
+
+        /// <summary>
+        /// new
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <exception cref="ArgumentNullException">callback is null.</exception>
+        public WatcherAction(Action<Data.WatchedEvent> callback)
+        {
+            if (callback == null) throw new ArgumentNullException("action");
+            this._callback = callback;
+        }
+
+        /// <summary>
+        /// invoke event.
+        /// </summary>
+        /// <param name="zevent"></param>
+        public void Invoke(Data.WatchedEvent zevent)
+        {
+            this._callback(zevent);
+        }
+    }
+}
